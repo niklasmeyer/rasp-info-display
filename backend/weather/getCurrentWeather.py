ca2bc71aa4188931da65c53fe7eaac1144045674
Nick <hi@nkls.cc>
2016-12-22 20:48:15 +0100

    cleanup

--- backend/weather/getCurrentWeather.py
+++ backend/weather/getCurrentWeather.py
@@ -1,41 +1,28 @@
 import requests
 import json
-from backend.weather.helper.functions import kelvinToCelsius, timestampToDatetime, meteorologyDegToWindDirection, metersPerSecondToKilometersPerHour
+from backend.weather.helper.functions import kelvinToCelsius, timestampToDatetime, meteorologyDegToWindDirection, \
+    metersPerSecondToKilometersPerHour
 
 cityId = 2944388  # bremen, germany
 apiKey = '2cc05d69d27887219e0bda4640ef59ac'
 apiUrl = 'http://api.openweathermap.org/data/2.5/weather?id=' + str(cityId) + '&APPID=' + apiKey
 
 def returnWeatherConditions():
-
     r = requests.get(apiUrl)
-
     APIResponse = json.loads(r.text)
-
-    temperature = kelvinToCelsius(APIResponse['main']['temp'])
-    sunrise = timestampToDatetime(APIResponse['sys']['sunrise'], '%H:%M')
-    sunset = timestampToDatetime(APIResponse['sys']['sunset'], '%H:%M')
-    weatherConditions = APIResponse['weather'][0]['main']
-    pressure = APIResponse['main']['pressure']
-    cloudyPercent = APIResponse['clouds']['all']
-    wind = {}
-    wind['direction'] = meteorologyDegToWindDirection(APIResponse['wind']['deg'])
-    wind['speed'] = metersPerSecondToKilometersPerHour(APIResponse['wind']['speed'])
-    humidity = APIResponse['main']['humidity']
-
 
     response = {}
     response['main'] = {}
-    response['main']['tempreature'] = temperature
-    response['main']['conditions'] = weatherConditions
-    response['main']['cloudyPercent'] = cloudyPercent
-    response['main']['pressure'] = pressure
-    response['main']['humidity'] = humidity
+    response['main']['tempreature'] = kelvinToCelsius(APIResponse['main']['temp'])
+    response['main']['conditions'] = APIResponse['weather'][0]['main']
+    response['main']['cloudyPercent'] = APIResponse['clouds']['all']
+    response['main']['pressure'] = APIResponse['main']['pressure']
+    response['main']['humidity'] = APIResponse['main']['humidity']
     response['daylight'] = {}
-    response['daylight']['sunset'] = sunset
-    response['daylight']['sunrise'] = sunrise
+    response['daylight']['sunrise'] = timestampToDatetime(APIResponse['sys']['sunrise'], '%H:%M')
+    response['daylight']['sunset'] = timestampToDatetime(APIResponse['sys']['sunset'], '%H:%M')
     response['wind'] = {}
-    response['wind']['direction'] = wind['direction']
-    response['wind']['speed'] = wind['speed']
+    response['wind']['direction'] = meteorologyDegToWindDirection(APIResponse['wind']['deg'])
+    response['wind']['speed'] = metersPerSecondToKilometersPerHour(APIResponse['wind']['speed'])
 
     return response
