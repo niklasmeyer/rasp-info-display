bc699e98839174743a207a4df44a1ba34bb7d53d
Nick <hi@nkls.cc>
2016-12-22 20:42:54 +0100

    weather done

---
+++ backend/weather/helper/functions.py
@@ -0,0 +1,18 @@
+import datetime as dt
+import math
+
+def kelvinToCelsius(temp):
+    return float(temp - 273.15)
+
+def timestampToDatetime(timestamp, format):
+    return dt.datetime.fromtimestamp(
+        int(timestamp)
+    ).strftime(format)
+
+def metersPerSecondToKilometersPerHour(ms):
+    return ms*3.6
+
+def meteorologyDegToWindDirection(deg):
+    windDirection = ['N', 'NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW']
+    index = round(deg/360*16);
+    return windDirection[0 if index == 16 else index]
